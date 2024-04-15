bdf4c129203e47d4b95072892c746d62b5024a51
admin <bekeriron@gmail.com>
2024-04-15 18:22:50 +0600

    Initial commit

---
+++ src/ddl_ten_table_console.sql
@@ -0,0 +1,82 @@
+create table football(
+                         player_name varchar ,
+                         player_age int
+
+);
+insert into football(player_name, player_age) VALUES ('Nurmuhammed ' ,17);
+
+create table flower(
+                       color varchar,
+                       price int
+);
+
+insert into flower (color, price) values ('red ' ,150);
+
+create table watch (
+                       size varchar,
+                       gender varchar
+);
+
+insert into  watch (size, gender) values ('l','male');
+
+create table monkeys (
+                         language varchar,
+                         color varchar
+);
+
+
+insert into monkeys (language, color) VALUES ('yaa-kuik','gray');
+
+create table lamp (
+                      color varchar,
+                      figure varchar
+);
+
+insert into  lamp (color, figure) VALUES ('yellow ','kub');
+
+create table Adyls (
+                       tol int,
+                       color varchar,
+                       barber varchar,
+                       brane varchar,
+                       born varchar
+
+);
+
+insert into Adyls(tol, color, barber, brane, born) VALUES (150,'niger','rap','null','trava');
+
+create table paints(
+                       color varchar,
+                       prise int
+
+);
+
+insert into paints(color, prise) VALUES ('gold',300);
+
+create table bag (
+                     size int,
+                     material varchar
+);
+
+insert into bag(size, material) VALUES ('2', 'leather');
+
+create table mess (
+                      beard varchar,
+                      speaks varchar
+);
+
+insert into mess(beard, speaks) VALUES ('black','much ');
+
+create table box(
+                    glass varchar,
+                    color varchar
+);
+insert into box(glass, color) VALUES ('sun glass','transparent');
+
+
+
+
+
+
+
+
---
+++ src/pgAdmin.sql
@@ -0,0 +1,40 @@
+insert into fox(color) values ('red');
+16:49:41
+create table fox( id serial primary key, color varchar );
+16:49:34
+insert into woters (prise ) values (35);
+16:46:40
+insert into woter (prise ) values (35);
+16:46:25
+create table woters ( id serial primary key , prise int );
+16:46:19
+insert into glass (prise ) values (3000);
+16:42:32
+create table glass ( id serial primary key , prise int );
+16:42:25
+insert into boxer(boxer_name ) values ('rocky');
+16:39:09
+insert into bexer(boxer_name ) values ('rocky');
+16:38:52
+create table boxer( id serial primary key, boxer_name varchar );
+16:38:40
+insert into laptop(model, color) values ('hp', 'gold');
+16:33:18
+insert into (model, color) values ('hp', 'gold');
+16:32:38
+create table laptop ( id serial primary key, model varchar, color varchar );
+16:31:59
+drop table laptop
+    16:31:50
+create table laptop ( id serial primary key, model varchar, color varchar );
+16:31:26
+create table laptop ( id serial primary key, model varchar, color varchar );
+16:31:09
+ insert into (model) values ('hp');
+16:30:11
+create table laptop ( id serial primary key, model varchar );
+16:29:15
+insert into phone (model) values ('samsung');
+16:27:09
+create table phone ( id serial primary key, model varchar );
+16:26:54
